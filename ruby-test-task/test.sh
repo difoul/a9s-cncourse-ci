fcbb5e866c232c52130b351f8ebe092f3b6f1cc7
difoul <foudil.bendjabeur@outlook.com>
2017-11-27 22:02:23 +0100

    Updating script for a9s test

--- ruby-test-task/test.sh
+++ ruby-test-task/test.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
-echo "toto"
-echo $MY_VAR
+set -x -e
+
+git clone https://github.com/a9s-examples/ruby-sinatra
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github"  | tar -zx
+mv cf /usr/local/bin
+cf --version
+cd ruby-sinatra
+cf api https://api.aws.ie.a9s.eu
+cf login -u ${cf_user} -p ${cf_password} -s ${cf_space} -o ${cf_org}
+cf push a9s-ruby-fbe2 --hostname a9s-ruby-fbe2
+[[ $(curl http://a9s-ruby-fbe2.aws.ie.a9sapp.eu |grep "Hello world" |wc -l) == 1 ]]
+cf delete a9s-ruby-fbe2 -f
+
